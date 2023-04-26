0d1158ec47bc29fa52adea55af2432cc8c622420
nelawarzecha <131850101+nelawarzecha@users.noreply.github.com>
2023-04-26 08:42:16 +0200

    Update kodzik.py

--- kodzik.py
+++ kodzik.py
@@ -2,4 +2,5 @@ def ostatni(lista):
     return lista[-1]
 lista=[1,5,7,9,10,2,3]
 ostatni_z_listy=ostatni(lista)
-print("ostatni element listy to:" ostatni_z_listy)
+print("nasza lista wygląda tak" lista)
+print("ostatni element tej listy to:" ostatni_z_listy)
